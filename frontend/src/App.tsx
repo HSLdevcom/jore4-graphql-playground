8d3464538232b971193f17992f8260039c399f40
haphut <haphut@mistmap.com>
2021-02-08 16:26:30 +0200

    Add ApolloProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
 import Map from "./components/Map";
 
+const client = new ApolloClient({
+  uri: "http://localhost:8080/v1/graphql",
+  cache: new InMemoryCache(),
+});
+
 const App: React.FC = () => (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateRows: "auto 1fr auto",
-    }}
-  >
-    <header>
-      <h1>Map example</h1>
-    </header>
-    <Map />
-    <footer>
-      <p>Footer</p>
-    </footer>
-  </div>
+  <ApolloProvider client={client}>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: "auto 1fr auto",
+      }}
+    >
+      <header>
+        <h1>Map example</h1>
+      </header>
+      <Map />
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </div>
+  </ApolloProvider>
 );
 
 export default App;
