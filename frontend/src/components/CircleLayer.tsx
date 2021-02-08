d14a5613605e8198225b91869769584b261de9e2
haphut <haphut@mistmap.com>
2021-02-08 17:07:33 +0200

    Query points from Hasura

--- frontend/src/components/CircleLayer.tsx
+++ frontend/src/components/CircleLayer.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { useMap, CircleMarker } from "react-leaflet";
+import { useQuery, gql } from "@apollo/client";
 
 interface CircleLayerProps {
   centerCoordinates?: string;
 }
 
+const ALL_POINTS = gql`
+  query GetAllPoints {
+    playground_points {
+      point_geog
+      point_id
+    }
+  }
+`;
+
 const CircleLayer: React.FC<CircleLayerProps> = (props) => {
   const map = useMap();
   console.log("map bounds:", map.getBounds());
 
-  return <CircleMarker center={[60.2, 24.94]} radius={20} />;
+  const { loading, error, data } = useQuery(ALL_POINTS);
+
+  if (loading) return null;
+  if (error) return null;
+
+  // FIXME any
+  return data.playground_points.map(
+    ({ point_id, point_geog: { coordinates } }: any) => (
+      <CircleMarker key={point_id} center={coordinates} radius={20} />
+    )
+  );
 };
 
 export default CircleLayer;
