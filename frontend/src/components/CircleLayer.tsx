f79bebdbab7af2a210d0c20041b8eb40ae1fd1ec
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-02-17 14:26:02 +0200

    Delete points when clicked

--- frontend/src/components/CircleLayer.tsx
+++ frontend/src/components/CircleLayer.tsx
@@ -1,11 +1,19 @@
+import { gql, useMutation, useSubscription } from "@apollo/client";
 import React from "react";
 import { CircleMarker } from "react-leaflet";
-import { gql, useSubscription } from "@apollo/client";
 
 const SUBSCRIBE_ALL_POINTS = gql`
   subscription SubscribeAllPoints {
     playground_points {
       point_geog
+      point_id
+    }
+  }
+`;
+
+const DELETE_POINT = gql`
+  mutation DeletePoint($point_id: uuid!) {
+    delete_playground_points_by_pk(point_id: $point_id) {
       point_id
     }
   }
@@ -17,6 +25,7 @@ const CircleLayer: React.FC = () => {
   //console.log("map bounds:", map.getBounds());
 
   const { loading, error, data } = useSubscription(SUBSCRIBE_ALL_POINTS);
+  const [deletePoint] = useMutation(DELETE_POINT);
 
   // FIXME: add spinner?
   if (loading) return null;
@@ -26,7 +35,21 @@ const CircleLayer: React.FC = () => {
   // FIXME: remove any type
   return data.playground_points.map(
     ({ point_id, point_geog: { coordinates } }: any) => (
-      <CircleMarker key={point_id} center={coordinates} radius={20} />
+      <CircleMarker
+        key={point_id}
+        center={coordinates}
+        radius={20}
+        eventHandlers={{
+          click: (e) => {
+            // Prevent click event from leaking into map layer
+            // FIXME: is there more elegant way for doing this?
+            // @ts-expect-error
+            e.originalEvent.view.L.DomEvent.stopPropagation(e);
+            console.log(`marked ${point_id} clicked, deleting it`, e);
+            deletePoint({ variables: { point_id } });
+          },
+        }}
+      />
     )
   );
 };
