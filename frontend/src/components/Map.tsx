d5d79f0fd9c6a1910b6cc867a08d1d745cec6480
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-02-18 08:42:47 +0200

    Fix missing marker icons

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,8 +1,18 @@
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/leaflet.css";
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import CircleLayer from "./CircleLayer";
 import EditLayer from "./EditLayer";
+
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 interface MapProps {
   centerCoordinates?: string;
